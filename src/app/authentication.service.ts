0e49180e18e3d928c4ff1fc4476c215196b9173f
Sittukala <60049724+Sittukala@users.noreply.github.com>
2022-11-30 00:13:46 +0000

    Initial commit

---
+++ src/app/authentication.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, map, Observable, of } from 'rxjs';
+import { ApiService, User } from './api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  private user$ = new BehaviorSubject<User>(null);
+
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  login(username: string, password: string): Observable<User | null> {
+    // your code here
+    console.log("user", username)
+    let loggedInUser = this.apiService.login(username,password)
+    if(loggedInUser){
+      
+      return loggedInUser
+    }
+  return of(null);
+  }
+
+  logout(): void {
+    this.user$.next(null);
+    this.router.navigateByUrl('/login');
+  }
+
+  get isAuthenticated(): Observable<boolean> {
+    return this.user$
+      .asObservable()
+      .pipe(map((u: User) => u?.isAuthenticated ?? false));
+  }
+}
